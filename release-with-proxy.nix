0e7abc945398dc1d921bc6a84aec4bb295348d7c
Sander van der Burg <svanderburg@gmail.com>
2011-01-30 17:22:44 +0000

    Added doc target

--- release-with-proxy.nix
+++ release-with-proxy.nix
@@ -22,6 +22,33 @@ let
 	src = disnix_proxy_example;
         inherit officialRelease;
       };
+
+    doc =
+      { tarball ? jobs.tarball {}
+      , system ? "x86_64-linux"
+      }:
+      
+      with import nixpkgs { inherit system; };
+      
+      releaseTools.nixBuild {
+        name = "disnix-proxy-example-doc";
+	version = builtins.readFile ./version;
+	src = tarball;
+	buildInputs = [ libxml2 libxslt dblatex tetex ];
+	
+	buildPhase = ''
+	  cd doc
+	  make docbookrng=${docbook5}/xml/rng/docbook docbookxsl=${docbook5_xsl}/xml/xsl/docbook
+	'';
+	
+	checkPhase = "true";
+	
+	installPhase = ''
+	  make DESTDIR=$out install
+	 
+	  echo "doc manual $out/share/doc/disnix-proxy-example/manual" >> $out/nix-support/hydra-build-products
+	'';
+      };
       
     build =
       { tarball ? jobs.tarball {}
